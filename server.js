6948a7a57ee29f35553a726c272eabf298f4b2fe
heju1201 <letrunghieuso4@gmail.com>
2024-09-12 16:58:20 +0700

    #17

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const app = express(); // app express
+const port = 8081; //port
 
+// khai báo route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/abc", (req, res) => {
+  res.send("check abc!");
+});
+app.get("/h1", (req, res) => {
+  res.send("<h1> check dung h1 </h1>");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
