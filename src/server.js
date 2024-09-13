24a059dacb7ccb3f2f4bda3aea2ad1c6a3585028
heju1201 <letrunghieuso4@gmail.com>
2024-09-13 21:03:22 +0700

    #19 ENV

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
+
+// console.log("check env : ", process.env);
+
 const app = express(); // app express
-const port = 8081; //port
+const port = process.env.PORT || 8888; //port
+const hostname = process.env.HOST_NAME;
 
 //config template engine
 app.set("views", path.join(__dirname, "views"));
@@ -19,6 +24,6 @@ app.get("/heju", (req, res) => {
   // res.send("<h1> check dung h1 </h1>");
   res.render("sample.ejs");
 });
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
