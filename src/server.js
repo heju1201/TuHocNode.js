0e8bc190fef94604b7fa66698b701e49ff1f11f7
heju1201 <letrunghieuso4@gmail.com>
2024-09-19 23:19:51 +0700

    connected database #30

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 require("dotenv").config();
 const configViewEngin = require("./config/viewsEngine");
 const webRouter = require("./routes/web");
+const mysql = require("mysql2");
 // console.log("check env : ", process.env);
 
 const app = express(); // app express
@@ -13,6 +14,22 @@ const hostname = process.env.HOST_NAME;
 app.use("/", webRouter);
 //config template engine
 configViewEngin(app);
+
+//test connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+// simple query
+// 'select * from `table` where `name` = "Page" AND `age` > 45',
+connection.query("select * from Users u", function (err, results, fields) {
+  console.log(results);
+  console.log(fields);
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
