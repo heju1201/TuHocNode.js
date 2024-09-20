f71a73129a99ec6826b32c80aa75fe6c18dfc44c
heju1201 <letrunghieuso4@gmail.com>
2024-09-20 10:53:17 +0700

    #33 connection Poll Pattern

--- src/config/database.js
+++ src/config/database.js
@@ -9,4 +9,15 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
+// const connection = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
 module.exports = connection;
