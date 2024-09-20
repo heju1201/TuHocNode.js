ce4aca0c51396b2a689d1c4a338c71dec5fa1222
heju1201 <letrunghieuso4@gmail.com>
2024-09-20 12:02:34 +0700

    Design Navber

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,14 +1,6 @@
 const connection = require("../config/database");
 const getHomePage = (req, res) => {
-  let users = [];
-  connection.query("select * from Users u", function (err, results, fields) {
-    users = results;
-    console.log(results);
-    // console.log(fields);
-    console.log("check users", users);
-
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 const getAbc = (req, res) => {
   res.send("check abc!");
